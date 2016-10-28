ec58a198a20dde93323612a94cdd9dda7d5f318c
Jonas Stams <jonasstams@gmail.com>
2016-10-28 18:37:57 +0200

    Admin user crud bezig

--- Werkpakket 2/src/AppBundle/Entity/User.php
+++ Werkpakket 2/src/AppBundle/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Table("users")
  * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\UserRepository")
  */
 class User implements UserInterface, \Serializable {
 
