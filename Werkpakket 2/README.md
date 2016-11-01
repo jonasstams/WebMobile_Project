b3774b12d3dce4543c0eb52a53d6053079e97c29
Pieter Bollen <pieter.bollen3@gmail.com>
2016-11-01 16:57:32 +0100

    admin page & coach aangepast voor customerController pdf voor reports van customers

--- Werkpakket 2/README.md
+++
@@ -1,4 +0,0 @@
-voorbeeldSecurityDoctrine
-=========================
-
-A Symfony project created on October 6, 2016, 12:26 am.
--- Werkpakket 2/web/js/modal.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Jonas on 19/10/2016.
- */
-$('#logoutModal').on('shown.bs.modal', function () {
-    $('#myInput').focus()
-});
